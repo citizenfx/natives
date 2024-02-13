75375bf77c873f65c57255de71f1991f83bfa6e2
thorium <102315529+thorium-cfx@users.noreply.github.com>
2024-02-13 12:04:09 +0100

    Central config file for both native and compatgen
    This allows us to easily change input and output file names throughout the whole native generation process.

---
+++ .ci/config.sh
@@ -0,0 +1,4 @@
+# default config values for our scripts. May be overriden in scripts, e.g.: manual workflow dispatch
+CONFIG_UPLOAD_COMPAT_FILE="natives_global_client_compat"
+CONFIG_UPLOAD_NATIVES_FILE="natives"
+CONFIG_COMPAT_FROM_DATE="2023-01-01" #YYYY-MM-DD
